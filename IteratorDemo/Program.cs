e3f74573806e63875e0dd58d3ad5b4a4a284a797
Jhon Calderón <jhoncv@digitalware.com.co>
2021-07-23 16:34:24 -0500

    first approach

---
+++ IteratorDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorDemo
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters) 
+        {
+            double bestPrice = 0;
+            IPainter cheapest = null;
+            foreach (IPainter painter in painters)
+            {
+                if (painter.IsAvailable)
+                {
+                    double price = painter.EstimateCompensation(sqMeters);
+                    if (cheapest == null || price < bestPrice)
+                    {
+                        cheapest = painter;
+                    }
+
+                }
+            }
+            return cheapest;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
