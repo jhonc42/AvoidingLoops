e8ccf8e3c2521006d7861fb4c7f41e3f1aa3d300
Jhon Calderón <jhoncv@digitalware.com.co>
2021-07-26 19:32:58 -0500

    solution with linq and occulting infrastructure code

---
+++ IteratorDemo/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorDemo
+{
+    public static class EnumerableExtensions
+    {
+        //Metodo generico que recibe una secuencia de elementos de tipo T:
+        // tambien habra algun tipo sobre que elementos de la secuencia se compararan, asi se pueden identificar y devolver el elemento con valor minimo
+        // y devuelve un tipo T que sera el objeto que tenga el valor minumo de la clave.
+        // el constraint que dice que T debe ser un tipo de referencia, porque se planea usar null para la semilla 
+        // OBSERVE QUE SE LLAMA DOS VECES AL METODO CRITERION QUE SE ENVIA POR PARAMETRO. ESTO ES REDUNDANTE Y PODRIA SER INEFICAZ, HAY UN PRINCIPIO QUE SE LLAMA
+        // EL PRINCIPIO DEL MENOR ASOMBRO O DE LA MENOR SORPRESA, Y ESTE METODO LO INCUMPLE, PORQUE PUEDE QUE LLAMEN AL SEGUNDO CRITERION O PUEDE QUE NO, SE DEBE MEJORAR ESTO
+        //public static T WithMinimun<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> criterion) 
+        //    where T : class
+        //    where TKey : IComparable<TKey> =>
+        //        sequence.Aggregate((T)null, (best, cur) =>
+        //            best == null || criterion(cur).CompareTo(criterion(best)) < 0 ? cur : best);
+
+        // ESTA SERIA LA SOLUCION AL CASO ANTERIOR, esta solucion es completamente ilegible, pero el autor dice que como va a nivel de infraestructura como 
+        // metodo de extension nadie la debe tomar en cuenta ni cambiarla, y el codigo que la consume osea el cliente allí si quedará legible.
+        public static T WithMinimun<T, TKey>(this IEnumerable<T> sequence, Func<T, TKey> criterion)
+            where T : class
+            where TKey : IComparable<TKey> =>
+                sequence
+                    .Select(obj => Tuple.Create(obj, criterion(obj)))
+                        .Aggregate((Tuple<T, TKey>)null,
+                            (best, cur) => best == null || cur.Item2.CompareTo(best.Item2) < 0 ? cur : best)
+                        .Item1;
+    }
+}
--- IteratorDemo/Program.cs
+++ IteratorDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IteratorDemo
 {
@@ -7,21 +8,59 @@ namespace IteratorDemo
     {
         private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters) 
         {
-            double bestPrice = 0;
-            IPainter cheapest = null;
-            foreach (IPainter painter in painters)
-            {
-                if (painter.IsAvailable)
-                {
-                    double price = painter.EstimateCompensation(sqMeters);
-                    if (cheapest == null || price < bestPrice)
-                    {
-                        cheapest = painter;
-                    }
 
-                }
-            }
-            return cheapest;
+            return
+                painters
+                .Where(x => x.IsAvailable)
+                .WithMinimun(painter => painter.EstimateCompensation(sqMeters));
+
+            // este c´ódigo es el mejor approach, pero es ilegible y viola ese principio de clean code, por lo cual, una forma de volver bonito y legible un código
+            // es envolviendo lo feo en un método de extensión:
+            //return
+            //    painters
+            //    .Where(x => x.IsAvailable)
+            //    .Aggregate((IPainter)null, (best, cur) =>
+            //        best == null ||
+            //        cur.EstimateCompensation(sqMeters) < best.EstimateCompensation(sqMeters) ?
+            //        cur : best
+            //    );
+
+            // este código es bueno, pero ilegible y llama dos veces a la función, además si viene un null se reventaría.
+            //return
+            //    painters
+            //    .Where(x => x.IsAvailable)
+            //    .Aggregate((best, cur) =>
+            //        best.EstimateCompensation(sqMeters) < cur.EstimateCompensation(sqMeters) ?
+            //        best : cur
+            //    );
+
+            // Este código no es bueno para buscar dentro de una secuencia de elementos, ya que afecta el performance
+            //return
+            //    painters
+            //    .Where(x => x.IsAvailable)
+            //    .OrderBy(painter => painter.EstimateCompensation(sqMeters))
+            //    .FirstOrDefault();
+
+            //return 
+            //    painters
+            //    .ThoseAvailable()
+            //    .WithMinimim(painter.EstimateCompensation(sqMeters));
+
+            //double bestPrice = 0;
+            //IPainter cheapest = null;
+            //foreach (IPainter painter in painters)
+            //{
+            //    if (painter.IsAvailable)
+            //    {
+            //        double price = painter.EstimateCompensation(sqMeters);
+            //        if (cheapest == null || price < bestPrice)
+            //        {
+            //            cheapest = painter;
+            //        }
+
+            //    }
+            //}
+            //return cheapest;
         }
         static void Main(string[] args)
         {
